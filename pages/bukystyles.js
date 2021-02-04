470b091375ca50376ab92094d87ff449e616dfb1
g-prog <alaobukky2@gmail.com>
2021-02-04 05:12:28 -0800

    POA style competition submission

--- pages/bukystyles.js
+++ pages/bukystyles.js
@@ -4,10 +4,20 @@ function bukystyles() {
     return (
         <div>
             <h1>Buky Style Page</h1>
-            <div class="shape">make me a different shape</div>
-            <div class="movement">animate me</div>
-            <div class="shadow">add shadows to me</div>
-            <div class="text">change this text to your name, then decorate it</div>
+            <div class="structure">
+            <div class={styles.shape4}></div>
+                <div class={styles.shape3}></div>
+                <div class={styles.shape2}></div>
+                <div class={styles.shape}></div> 
+            </div>
+            
+            <div class={styles.movement}><h3>Animate me</h3></div>
+            <div class={styles.shadow}><h1> What a shadowy effect</h1></div>
+            <div class={styles.text}>
+                <h5>I was decorated quite nicely</h5>
+                <h4>I was decorated quite nicely</h4>
+                <h3>I was decorated quite nicely</h3>
+            </div>
         </div>
     )
 }
