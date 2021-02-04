96b227ee99eed6a420d31986255a816096665e9e
James Dunseith <info@workshopmaybe.com>
2021-02-04 15:29:20 -0500

    Add my styles

--- pages/jamesstyles.js
+++ pages/jamesstyles.js
@@ -4,10 +4,22 @@ function jamesstyles() {
     return (
         <div>
             <h1>James Style Page</h1>
-            <div class={styles.shape}>make me a different shape</div>
-            <div class={styles.movement}>animate me</div>
-            <div class={styles.shadow}>add shadows to me</div>
-            <div class={styles.text}>change this text to your name, then decorate it</div>
+            <div class={styles.shape}>
+                <p class={styles.shapetext}>
+                    I'm a new shape!
+                </p>
+                <p>
+                    <a href='https://bennettfeely.com/clippy/' class={styles.jameslinks}>
+                        CSS clip-path maker
+                    </a>
+                </p>
+            </div>
+            <div class={styles.movement}>can't stop won't stop</div>
+            <div class={styles.shadow}>what's in the shade there?</div>
+            <div class={styles.text}>
+                <p>James</p>
+                <p><a href='https://fonts.google.com/' class={styles.jameslinks}>Google Fonts</a></p>
+            </div>
         </div>
     )
 }
